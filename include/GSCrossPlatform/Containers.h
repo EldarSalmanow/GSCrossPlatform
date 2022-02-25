a10bae1009301c2626da6fa94efb698aabb4344d
EldarSalmanow <eldar.salm@gmail.com>
2022-02-25 14:18:06 +0300

    Adding containers module file

---
+++ include/GSCrossPlatform/Containers.h
@@ -0,0 +1,12 @@
+#ifndef GSCROSSPLATFORM_CONTAINERS_H
+#define GSCROSSPLATFORM_CONTAINERS_H
+
+/**
+ * This file include CrossPlatform Containers Module
+ */
+
+#include "GSCrossPlatform/Containers/Iterator.h"
+
+#include <GSCrossPlatform/Containers/String.h>
+
+#endif //GSCROSSPLATFORM_CONTAINERS_H
