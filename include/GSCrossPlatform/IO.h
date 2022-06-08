7e1898f10bd7b16c17dfbf0409e2cfb1b134391e
Eldar <eldar.salm@gmail.com>
2022-06-08 13:25:53 +0300

    Updating IO module

--- include/GSCrossPlatform/IO.h
+++ include/GSCrossPlatform/IO.h
@@ -5,29 +5,53 @@
 
 #include <GSCrossPlatform/UString.h>
 
+inline LRef<std::istream> operator>>(LRef<std::istream> stream, LRef<USymbol> symbol) {
+    auto byte = StaticCast<U8>(stream.get());
+
+    if (stream.eof()) {
+        return stream;
+    }
+
+    auto symbolSize = UTF8Size(byte);
+
+    Vector<U8> bytes;
+
+    bytes.Append(byte);
+
+    for (U64 i = 1; i < symbolSize; ++i) {
+        byte = StaticCast<U8>(stream.get());
+
+        bytes.Append(byte);
+    }
+
+    auto codePoint = FromUTF8(bytes);
+
+    symbol = USymbol(codePoint);
+
+    return stream;
+}
+
+inline LRef<std::ostream> operator<<(LRef<std::ostream> stream, ConstLRef<USymbol> symbol) {
+    auto bytes = ToUTF8(symbol.CodePoint());
+
+    for (auto &byte : bytes) {
+        stream.put(StaticCast<C>(byte));
+    }
+
+    return stream;
+}
+
 inline LRef<std::istream> operator>>(LRef<std::istream> stream, LRef<UString> string) {
     while (true) {
-        auto byte = StaticCast<U8>(stream.get());
+        USymbol symbol;
 
-        if (byte == '\n' || stream.eof()) {
+        stream >> symbol;
+
+        if (symbol == '\n' || stream.eof()) {
             break;
         }
 
-        auto symbolSize = UTF8Size(byte);
-
-        Vector<U8> bytes;
-
-        bytes.Append(byte);
-
-        for (U64 i = 1; i < symbolSize; ++i) {
-            byte = StaticCast<U8>(stream.get());
-
-            bytes.Append(byte);
-        }
-
-        auto codePoint = FromUTF8(bytes);
-
-        string.Append(USymbol(codePoint));
+        string.Append(symbol);
     }
 
     return stream;
@@ -35,11 +59,7 @@ inline LRef<std::istream> operator>>(LRef<std::istream> stream, LRef<UString> st
 
 inline LRef<std::ostream> operator<<(LRef<std::ostream> stream, ConstLRef<UString> string) {
     for (auto &symbol : string) {
-        auto bytes = symbol.AsUTF8();
-
-        for (auto &byte : bytes) {
-            stream << byte;
-        }
+        stream << symbol;
     }
 
     return stream;
